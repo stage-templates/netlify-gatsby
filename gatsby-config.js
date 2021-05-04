ec20a084c1e4041c0c444848b9d286ceb72d0e4e
Steven Klein <steven.p.klein@gmail.com>
2021-05-04 18:34:59 -0500

    fix: deployments?

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,12 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Some Person`,
+      summary: `just another gatsby site.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    siteUrl: `https://gatsby.sklein.dev/`,
     social: {
-      twitter: `kylemathews`,
     },
   },
   plugins: [
